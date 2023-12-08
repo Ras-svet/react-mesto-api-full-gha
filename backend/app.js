99461f4b46186542b30454436f78f1b0f11723e0
Ras-svet <kulagina.cveta@yandex.ru>
2023-12-08 19:22:00 +0300

    попытка

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 require('dotenv').config();
 
 const app = express();
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
