a08f625170ab64bd29d4ac49d9ad645b265a30c8
Ras-svet <kulagina.cveta@yandex.ru>
2023-12-10 16:26:21 +0300

    попытка

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -6,7 +6,7 @@ const ForbiddenError = require('../errors/forbidden-error');
 
 module.exports.getCards = (req, res, next) => {
   Card.find({})
-    .then((cards) => res.status(200).send({ cards }))
+    .then((cards) => res.status(200).send(cards))
     .catch((err) => next(err));
 };
 
